992bee60763b7334d5a57f21c4b885268a2a573e
Estifki <estif0919@gmail.com>
2023-04-21 18:59:07 +0300

    updated ui

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -103,111 +103,140 @@ class HomeScreen extends StatelessWidget {
           //
           //
           //
+          //
+          //
+          //
+          //
+          //
           Container(
             width: screenSize.width,
             decoration: BoxDecoration(
                 color: Colors.grey.shade100,
                 borderRadius:
                     const BorderRadius.vertical(top: Radius.circular(40.0))),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 25, right: 25, left: 25),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          PaymentWidgets(
-                              title: "Pay", icon: Icons.clean_hands_rounded),
-                          PaymentWidgets(
-                              title: "Scan",
-                              icon: Icons.qr_code_scanner_rounded),
-                        ]),
-                    const SizedBox(height: 20),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          PaymentWidgets(
-                              title: "Pay", icon: Icons.clean_hands_rounded),
-                          PaymentWidgets(
-                              title: "Scan",
-                              icon: Icons.qr_code_scanner_rounded),
-                        ]),
-
-                    const SizedBox(height: 40),
-                    //
-                    //Header
-                    //
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: const [
-                          Text(
-                            "Transaction History",
-                            style: TextStyle(
-                                color: Color(0xff5d35d6),
-                                fontWeight: FontWeight.w600),
-                          ),
-                          Text("See all"),
-                        ]),
-                    const SizedBox(height: 10),
-                    //
-                    //
-                    //
-                    const TransactionHistoryWidget(),
-                    const SizedBox(height: 10),
-                    const TransactionHistoryWidget(),
-
-                    const SizedBox(height: 10),
-                    const TransactionHistoryWidget(),
-                    SizedBox(height: 10)
-                  ]),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 25, right: 25, left: 25),
+              child: BottomWidget(),
             ),
           )
         ]),
       ),
+      //
+      //
+      //
       bottomNavigationBar: BottomNavigationBar(
         elevation: 0.0,
         type: BottomNavigationBarType.fixed,
         selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
         unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-        selectedItemColor: Colors.indigo,
+        selectedItemColor: const Color(0xff5d35d6),
         unselectedItemColor: Colors.grey,
         backgroundColor: Colors.grey.shade100,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_outlined,
-              size: 27,
-            ),
-            label: "Home",
-          ),
+              icon: Icon(Icons.home_outlined, size: 27), label: "Home"),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.wallet_giftcard_outlined,
-                size: 27,
-              ),
-              label: "Transaction"),
+              icon: Icon(Icons.sync_alt_sharp, size: 27), label: "Transaction"),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.favorite_outline,
-                size: 27,
-              ),
-              label: "Help"),
+              icon: Icon(Icons.help_outline_sharp, size: 27), label: "Help"),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person_outline,
-                size: 27,
-              ),
-              label: "Profile"),
+              icon: Icon(Icons.person_outline, size: 27), label: "Profile"),
         ],
       ),
     );
   }
 }
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+class BottomWidget extends StatelessWidget {
+  const BottomWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                PaymentWidgets(title: "Pay", icon: Icons.clean_hands_rounded),
+                PaymentWidgets(
+                    title: "Scan", icon: Icons.qr_code_scanner_rounded),
+              ]),
+          const SizedBox(height: 20),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                PaymentWidgets(
+                    title: "Withdraw", icon: Icons.clean_hands_rounded),
+                PaymentWidgets(
+                    title: "Pay Bills", icon: Icons.spatial_audio_off_rounded),
+              ]),
+
+          const SizedBox(height: 40),
+          //
+          //Header
+          //
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  "Transaction History",
+                  style: TextStyle(
+                      color: Color(0xff5d35d6), fontWeight: FontWeight.w600),
+                ),
+                Text("See all"),
+              ]),
+          const SizedBox(height: 10),
+          //
+          //
+          //
+          const TransactionHistoryWidget(),
+          const SizedBox(height: 10),
+          const TransactionHistoryWidget(),
+
+          const SizedBox(height: 10),
+          const TransactionHistoryWidget(),
+          const SizedBox(height: 10)
+        ]);
+  }
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 class TransactionHistoryWidget extends StatelessWidget {
   const TransactionHistoryWidget({super.key});
@@ -224,8 +253,16 @@ class TransactionHistoryWidget extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(width: 10),
-            Image.asset("assets/whit2.jpg", height: 40),
+            const SizedBox(width: 15),
+            Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey.withOpacity(0.1),
+                ),
+                child: Center(
+                    child: Image.asset("assets/ethio_tele.jpg", height: 20))),
             const SizedBox(width: 10),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -234,20 +271,22 @@ class TransactionHistoryWidget extends StatelessWidget {
                 Text(
                   "Ethio telecom bill",
                   style: TextStyle(
-                      fontSize: 15,
+                      fontSize: 14,
                       fontWeight: FontWeight.bold,
                       color: Colors.black),
                 ),
-                SizedBox(height: 2),
+                SizedBox(height: 3),
                 Text(
                   "Utility Bill",
                   style: TextStyle(
-                      color: Color(0xff5d35d6), fontWeight: FontWeight.w600),
+                      fontSize: 13,
+                      color: Color(0xff5d35d6),
+                      fontWeight: FontWeight.w600),
                 ),
-                SizedBox(height: 2),
+                SizedBox(height: 3),
                 Text(
                   "Date 20/12/23",
-                  style: TextStyle(color: Colors.black45, fontSize: 12),
+                  style: TextStyle(fontSize: 13, color: Colors.black45),
                 ),
               ],
             ),
@@ -261,12 +300,16 @@ class TransactionHistoryWidget extends StatelessWidget {
                   Text(
                     "5600.00",
                     style: TextStyle(
-                        color: Color(0xff5d35d6), fontWeight: FontWeight.bold),
+                        color: Color(0xff5d35d6),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14),
                   ),
                   Text(
                     "ETB",
                     style: TextStyle(
-                        color: Color(0xff5d35d6), fontWeight: FontWeight.bold),
+                        color: Color(0xff5d35d6),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14),
                   ),
                 ],
               ),
@@ -276,6 +319,20 @@ class TransactionHistoryWidget extends StatelessWidget {
   }
 }
 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 class PaymentWidgets extends StatelessWidget {
   final String title;
   final IconData icon;
@@ -288,7 +345,7 @@ class PaymentWidgets extends StatelessWidget {
       width: screenSize.width * 0.41,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
-        color: Color.fromARGB(255, 224, 218, 235),
+        color: const Color.fromARGB(255, 224, 218, 235),
       ),
       child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
